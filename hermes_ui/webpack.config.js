43584dd658d11b188d8a27041c878d4e1d3a37a5
TAHRI Ahmed R <nyuubi.10@gmail.com>
2019-11-25 09:01:11 +0100

    Chgmt font relatif dir webpack

--- hermes_ui/webpack.config.js
+++ hermes_ui/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
                 loader: 'url-loader',
                 options: {
                     limit: 10000,
-                    name: 'build/fonts/[name].[hash:7].[ext]'
+                    name: 'fonts/[name].[hash:7].[ext]'
                 }
             },
             {
