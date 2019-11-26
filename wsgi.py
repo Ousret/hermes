4b6db17ffb91136283804ef0fb5bb1dbc3bbf98c
TAHRI Ahmed R <nyuubi.10@gmail.com>
2019-11-26 10:55:06 +0100

    Support SSL avec Flask

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,6 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=5000,
-        threaded=True
+        threaded=True,
+        ssl_context=(app.config.get('HERMES_CERTIFICAT_TLS'), app.config.get('HERMES_CLE_PRIVEE_TLS')) if app.config.get('HERMES_CERTIFICAT_TLS') and app.config.get('HERMES_CLE_PRIVEE_TLS') else None
     )
