fdfc37a49e5fd689ea33099aeffaaa10c1e9680d
TAHRI Ahmed R <nyuubi.10@gmail.com>
2019-11-26 11:34:37 +0100

    load context default cert server auth Flask

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from hermes_ui.app import app
-from ssl import SSLContext, PROTOCOL_TLS, CERT_REQUIRED
+from ssl import SSLContext, PROTOCOL_TLS, CERT_REQUIRED, Purpose
 
 if __name__ == '__main__':
 
@@ -9,14 +9,17 @@ if __name__ == '__main__':
     if app.config.get('HERMES_CERTIFICAT_TLS') and app.config.get('HERMES_CLE_PRIVEE_TLS'):
         context = SSLContext(PROTOCOL_TLS)
 
+        context.check_hostname = True
+        context.set_ciphers(
+            "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384")
+
         context.verify_mode = CERT_REQUIRED
         context.load_cert_chain(app.config.get('HERMES_CERTIFICAT_TLS'), app.config.get('HERMES_CLE_PRIVEE_TLS'))
 
+        context.load_default_certs(Purpose.SERVER_AUTH)
+
         if app.config.get('HERMES_CERTIFICAT_CA'):
             context.load_verify_locations(app.config.get('HERMES_CERTIFICAT_CA'))
-
-    if context:
-        context.set_ciphers("ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384")
 
     app.run(
         host='0.0.0.0',
