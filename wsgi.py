9d1ac9784f899f46e01165aeaf496b2cf032d6e4
TAHRI Ahmed R <nyuubi.10@gmail.com>
2019-11-26 11:13:39 +0100

    Support TLS avec cert ca bundle uniquement

--- wsgi.py
+++ wsgi.py
@@ -6,13 +6,18 @@ if __name__ == '__main__':
 
     context = None
 
-    if app.config.get('HERMES_CERTIFICAT_TLS'):
-
+    if app.config.get('HERMES_CERTIFICAT_TLS') and app.config.get('HERMES_CLE_PRIVEE_TLS') and app.config.get('HERMES_CERTIFICAT_CA'):
         context = SSLContext(PROTOCOL_TLSv1_2)
 
         context.verify_mode = CERT_REQUIRED
         context.load_cert_chain(app.config.get('HERMES_CERTIFICAT_TLS'), app.config.get('HERMES_CLE_PRIVEE_TLS'))
         context.load_verify_locations(app.config.get('HERMES_CERTIFICAT_CA'))
+
+    elif app.config.get('HERMES_CERTIFICAT_CA') and app.config.get('HERMES_CLE_PRIVEE_TLS'):
+        context = SSLContext(PROTOCOL_TLSv1_2)
+
+        context.verify_mode = CERT_REQUIRED
+        context.load_cert_chain(app.config.get('HERMES_CERTIFICAT_CA'), app.config.get('HERMES_CLE_PRIVEE_TLS'))
 
     app.run(
         host='0.0.0.0',
