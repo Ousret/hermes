f33c898751d3409072a4cc48ddc967af0c9256dd
TAHRI Ahmed R <nyuubi.10@gmail.com>
2019-11-26 11:09:00 +0100

    Support chargement CA pem Flask

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 from hermes_ui.app import app
+from ssl import SSLContext, PROTOCOL_TLSv1_2, CERT_REQUIRED
 
 if __name__ == '__main__':
+
+    context = None
+
+    if app.config.get('HERMES_CERTIFICAT_TLS'):
+
+        context = SSLContext(PROTOCOL_TLSv1_2)
+
+        context.verify_mode = CERT_REQUIRED
+        context.load_cert_chain(app.config.get('HERMES_CERTIFICAT_TLS'), app.config.get('HERMES_CLE_PRIVEE_TLS'))
+        context.load_verify_locations(app.config.get('HERMES_CERTIFICAT_CA'))
 
     app.run(
         host='0.0.0.0',
         port=5000,
         threaded=True,
-        ssl_context=(app.config.get('HERMES_CERTIFICAT_TLS'), app.config.get('HERMES_CLE_PRIVEE_TLS')) if app.config.get('HERMES_CERTIFICAT_TLS') and app.config.get('HERMES_CLE_PRIVEE_TLS') else None
+        ssl_context=context
     )
