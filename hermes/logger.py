94cf74e228af5a0de90aa35c2fc1c23d4529b4c9
TAHRI Ahmed R <nyuubi.10@gmail.com>
2019-11-29 11:27:54 +0100

    Activation de la couleur sur le sink mem_handler
    loguru

--- hermes/logger.py
+++ hermes/logger.py
@@ -1,9 +1,9 @@
 from os.path import dirname, realpath
 from logging.handlers import BufferingHandler
 
-from loguru import logger, _defaults
+from loguru import logger
 
 __path__ = dirname(realpath(__file__))
 
 mem_handler = BufferingHandler(capacity=15000)
-logger.add(mem_handler, colorize=_defaults.LOGURU_COLORIZE, level='INFO')
+logger.add(mem_handler, colorize=True, level='INFO', enqueue=False)
